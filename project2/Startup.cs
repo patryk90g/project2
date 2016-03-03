a2ff8a32a42b990ab16e4ffa1aa5424f3313b2f1
Patryk Gorecki <patryk90g@gmail.com>
2016-03-03 20:45:10 +0100

    3,2,1..New project

---
+++ project2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(project2.Startup))]
+namespace project2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
